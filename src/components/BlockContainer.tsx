7e7defe4c8cc64c17626c783fe93419df41ebe4a
SadMearise <nikita.khrustalev2012@gmail.com>
2023-12-16 18:26:50 +0300

    feat: implement default block container

---
+++ src/components/BlockContainer.tsx
@@ -0,0 +1,15 @@
+import { FC, PropsWithChildren } from "react";
+
+type BlockContainerProps = {
+  styles?: string;
+};
+
+const classes: Record<string, string> = {
+  block: "bg-dark-100 rounded-lg",
+};
+
+const BlockContainer: FC<PropsWithChildren<BlockContainerProps>> = ({ styles, children }) => {
+  return <div className={`${classes.block} ${styles}`}>{children}</div>;
+};
+
+export default BlockContainer;
