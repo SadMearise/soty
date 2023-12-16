474deae874d87a1dae8b1bda73673a9194a33658
SadMearise <nikita.khrustalev2012@gmail.com>
2023-12-16 18:24:23 +0300

    feat: implement icon button

---
+++ src/components/IconButton.tsx
@@ -0,0 +1,28 @@
+import { FC, PropsWithChildren } from "react";
+import BaseButton from "./BaseButton";
+
+type IconButtonProps = {
+  action: () => void;
+  label: string;
+  disabled?: boolean;
+};
+
+const classes: Record<string, string> = {
+  button: "relative w-8 h-8 bg-black/70 rounded-full cursor-pointer disabled:cursor-not-allowed disabled:opacity-60",
+  icon: "absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] w-4 h-auto fill-white",
+};
+
+const IconButton: FC<PropsWithChildren<IconButtonProps>> = ({ children, action, label, disabled }) => {
+  return (
+    <BaseButton
+      label={label}
+      styles={classes.button}
+      action={action}
+      disabled={disabled}
+    >
+      <span className={classes.icon}>{children}</span>
+    </BaseButton>
+  );
+};
+
+export default IconButton;
