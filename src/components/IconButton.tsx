cf4cc3218354cddc9ba067569b44244f965775f4
SadMearise <nikita.khrustalev2012@gmail.com>
2023-12-25 17:53:53 +0300

    fix: change component type

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -2,14 +2,14 @@ import { FC, PropsWithChildren } from "react";
 import BaseButton from "./BaseButton";
 
 type IconButtonProps = {
-  action: () => void;
   label: string;
+  action?: () => void;
   disabled?: boolean;
 };
 
 const classes: Record<string, string> = {
   button: "relative w-8 h-8 bg-black/70 rounded-full cursor-pointer disabled:cursor-not-allowed disabled:opacity-60",
-  icon: "absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] w-4 h-auto fill-white",
+  icon: "absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] w-4 h-auto fill-white text-sm",
 };
 
 const IconButton: FC<PropsWithChildren<IconButtonProps>> = ({ children, action, label, disabled }) => {
