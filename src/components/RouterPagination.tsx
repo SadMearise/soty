12a6fccfcbcf993f9208c2517652ec4547d1c904
SadMearise <nikita.khrustalev2012@gmail.com>
2023-12-25 17:55:47 +0300

    feat: update pagination component according new tooltip

--- src/components/RouterPagination.tsx
+++ src/components/RouterPagination.tsx
@@ -4,6 +4,7 @@ import Pagination from "./Pagination";
 import withTooltip from "../hocs/withTooltip";
 import IconButton from "./IconButton";
 import { TooltipPosition } from "../utils/constants";
+import Tooltip from "./Tooltip";
 
 type RouterPaginationProps = {
   startedHistoryLength: number;
@@ -24,66 +25,66 @@ const RouterPagination: FC<RouterPaginationProps> = ({ startedHistoryLength }) =
     navigate(1);
   };
 
-  const prevButton = (
-    <IconButton
-      action={goBack}
-      disabled={prevButtonIsDisabled}
-      label="pagination-prev"
-    >
-      <svg
-        data-encore-id="icon"
-        role="img"
-        aria-hidden="true"
-        viewBox="0 0 16 16"
+  const prevButton = () => {
+    return (
+      <IconButton
+        action={goBack}
+        disabled={prevButtonIsDisabled}
+        label="pagination-prev"
       >
-        <path d="M11.03.47a.75.75 0 0 1 0 1.06L4.56 8l6.47 6.47a.75.75 0 1 1-1.06 1.06L2.44 8 9.97.47a.75.75 0 0 1 1.06 0z" />
-      </svg>
-    </IconButton>
-  );
+        <svg
+          data-encore-id="icon"
+          role="img"
+          aria-hidden="true"
+          viewBox="0 0 16 16"
+        >
+          <path d="M11.03.47a.75.75 0 0 1 0 1.06L4.56 8l6.47 6.47a.75.75 0 1 1-1.06 1.06L2.44 8 9.97.47a.75.75 0 0 1 1.06 0z" />
+        </svg>
+      </IconButton>
+    );
+  };
 
-  const nextButton = (
-    <IconButton
-      action={goForward}
-      disabled={nextButtonIsDisabled}
-      label="pagination-next"
-    >
-      <svg
-        data-encore-id="icon"
-        role="img"
-        aria-hidden="true"
-        viewBox="0 0 16 16"
+  const nextButton = () => {
+    return (
+      <IconButton
+        action={goForward}
+        disabled={nextButtonIsDisabled}
+        label="pagination-next"
       >
-        <path d="M4.97.47a.75.75 0 0 0 0 1.06L11.44 8l-6.47 6.47a.75.75 0 1 0 1.06 1.06L13.56 8 6.03.47a.75.75 0 0 0-1.06 0z" />
-      </svg>
-    </IconButton>
-  );
+        <svg
+          data-encore-id="icon"
+          role="img"
+          aria-hidden="true"
+          viewBox="0 0 16 16"
+        >
+          <path d="M4.97.47a.75.75 0 0 0 0 1.06L11.44 8l-6.47 6.47a.75.75 0 1 0 1.06 1.06L13.56 8 6.03.47a.75.75 0 0 0-1.06 0z" />
+        </svg>
+      </IconButton>
+    );
+  };
 
-  const PrevButtonWithTooltip = withTooltip(() => {
-    return prevButton;
-  });
-  const NextButtonWithTooltip = withTooltip(() => {
-    return nextButton;
-  });
+  const PrevButtonWithTooltip = withTooltip(prevButton, Tooltip);
+  const NextButtonWithTooltip = withTooltip(nextButton, Tooltip);
 
   return (
     <Pagination
       prevButton={
         prevButtonIsDisabled ? (
-          prevButton
+          prevButton()
         ) : (
           <PrevButtonWithTooltip
-            tooltipText="Назад"
             position={TooltipPosition.Bottom}
+            tooltipText="Назад"
           />
         )
       }
       nextButton={
         nextButtonIsDisabled ? (
-          nextButton
+          nextButton()
         ) : (
           <NextButtonWithTooltip
-            tooltipText="Вперёд"
             position={TooltipPosition.Bottom}
+            tooltipText="Вперёд"
           />
         )
       }
