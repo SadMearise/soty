4580be22e5803d8d54c03f327143c490aa642019
SadMearise <nikita.khrustalev2012@gmail.com>
2024-07-19 00:10:07 +0300

    fix: change tooltip display

--- src/components/EverydayPlaylists/EverydayPlaylistCard.tsx
+++ src/components/EverydayPlaylists/EverydayPlaylistCard.tsx
@@ -26,13 +26,13 @@ const EverydayPlaylistCard: FC<EverydayPlaylistProps> = ({ type, id, imageUrl, n
   const playlistIsPlaying = isPlaying && playingPlaylistId === id;
 
   const classes = {
-    item: "relative flex rounded-[4px] bg-white/[.07] overflow-hidden transition-colors hover:bg-white/[.2] group",
+    item: "relative flex rounded-[4px] bg-white/[.07] transition-colors hover:bg-white/[.2] items-center group",
     itemLink: "absolute w-full h-full top-0 left-0",
-    leftSide: "relative h-full aspect-square",
-    leftSideImage: "block absolute h-full w-full top-0 left-0 object-cover",
-    rightSide: "flex flex-auto justify-between items-center px-[16px] overflow-hidden",
-    rightSideWrapper: "w-12 h-12 2xl-max:w-9 2xl-max:h-9",
-    rightSideText: "text-white text-sm font-bold truncate xl-min:text-base",
+    leftCol: "relative h-full aspect-square",
+    leftColImage: "block absolute h-full w-full top-0 left-0 object-cover",
+    centerCol: "flex flex-auto items-center px-[16px] overflow-hidden",
+    centerColText: "text-white text-sm font-bold truncate xl-min:text-base",
+    rightColWrapper: "w-12 h-12 2xl-max:w-9 2xl-max:h-9 mr-[16px]",
   };
 
   const handlePlayback = async () => {
@@ -51,34 +51,34 @@ const EverydayPlaylistCard: FC<EverydayPlaylistProps> = ({ type, id, imageUrl, n
   return (
     id && (
       <article className={classes.item}>
-        <div className={classes.leftSide}>
+        <div className={classes.leftCol}>
           <img
             src={imageUrl}
-            className={classes.leftSideImage}
+            className={classes.leftColImage}
           />
         </div>
-        <div className={classes.rightSide}>
-          <span className={classes.rightSideText}>{name}</span>
-          <div className={classes.rightSideWrapper}>
-            <PlaybackWithEqualizer
-              isPlaying={playlistIsPlaying}
-              equalizer={{
-                id: "equalizer",
-                colorFill: "fill-green-100",
-                size: "16px",
-              }}
-              playback={{
-                isPlaying: playlistIsPlaying,
-                variant: "rounded",
-                roundedButtonSize: RoundedButtonSize.MdAdaptive,
-                roundedButtonColor: RoundedButtonColor.Green,
-                iconColorFill: "fill-black",
-                onClick: handlePlayback,
-                shadow: true,
-              }}
-              playbackVisibility
-            />
-          </div>
+        <div className={classes.centerCol}>
+          <span className={classes.centerColText}>{name}</span>
+        </div>
+        <div className={classes.rightColWrapper}>
+          <PlaybackWithEqualizer
+            isPlaying={playlistIsPlaying}
+            equalizer={{
+              id: "equalizer",
+              colorFill: "fill-green-100",
+              size: "16px",
+            }}
+            playback={{
+              isPlaying: playlistIsPlaying,
+              variant: "rounded",
+              roundedButtonSize: RoundedButtonSize.MdAdaptive,
+              roundedButtonColor: RoundedButtonColor.Green,
+              iconColorFill: "fill-black",
+              onClick: handlePlayback,
+              shadow: true,
+            }}
+            playbackVisibility
+          />
         </div>
         <Link
           to={`/${LINKS[type].route}/${id}`}
