1e36eced82d14d694e39cd0cb26fc3e1425addd4
SadMearise <nikita.khrustalev2012@gmail.com>
2024-07-19 13:33:12 +0300

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "./soty",
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "./",
+  };
+
+  if (command === "build") {
+    config.base = "/soty/";
+  }
+
+  return config;
 });
